4442db3b62eefd236e247fc07e885dc710acb8b8
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-03-03 15:53:56 -0600

    2nd commit

---
+++ Application.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Application {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			Menu menu = new Menu();
+			menu.start();
+	}
+
+}
