e31ced5b7a4ef0fcf6d839cfebdf77123b1645a0
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-03-06 23:48:28 -0600

    Update TeamsDao.java
    added descriptions, adjusted format, changed package names

--- TeamsDao.java
+++ TeamsDao.java
@@ -1,60 +1,84 @@
-package dao;
+package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import entity.Teams;
-
+import Entity.Teams;
 
 public class TeamsDao {
 	
+	private final String addteam = "Insert into teams(teamId, team_name, team_champs, divisionId) Values(?, ?, ?, ?)";
+	private final String deleteteam = "Delete from teams where teamId =?";
+	private final String updateteam = "Update teams set team_name=?, team_champs=?, divisionId=? where teamId=?";
 	private Connection connection;
-	private PlayersDao playerDao;
-
 	
-	public TeamsDao() {
-		connection = DBConnection.getConnection();
-		playerDao = new PlayersDao();
+	public TeamsDao() { //constructor
+		connection = DBConnection.getInstance().getConnection(); //instance of connection
 	}
 	
-	public List<Teams> getTeams() throws SQLException {
-		ResultSet rs = connection.prepareStatement("SELECT * FROM teams").executeQuery();
-		List<Teams> teams = new ArrayList<Teams>();
+	public List<Teams> getAllTeams() throws SQLException{ //gets all teams on the db, returns them as a list
+		List<Teams> out = new ArrayList<>();
 		
-		while (rs.next()) {
-			teams.add(populateTeam(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4)));
+		Statement s = connection.createStatement();
+		ResultSet rs = s.executeQuery( "Select * from teams" );
+		
+		while ( rs.next() ) {
+			out.add( new Teams(rs.getInt( "teamId" ), rs.getString( "team_name" ), rs.getInt( "team_champs" ), rs.getInt( "divisionId" ) ));
 			
 		}
-		
-		return teams;
+		return out;
 	}
 	
-	public Teams getTeamById(int id) throws SQLException {
-		PreparedStatement ps = connection.prepareStatement("SELECT * FROM teams WHERE id = ?");
-		ps.setInt(1,  id);
+	public void addTeam(int teamId, String team_name, int team_champs, int divisionId) { //creates a new team
+		try ( PreparedStatement ps = connection.prepareStatement(addteam)){
+			ps.setInt(1, teamId);
+			ps.setString(2, team_name);
+			ps.setInt(3, team_champs);
+			ps.setInt(4, divisionId);
+			ps.executeUpdate();
+		}
+		catch( SQLException e ) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteTeam(int teamId) throws SQLException { //deletes a team based off team id
+		try ( PreparedStatement ps = connection.prepareStatement(deleteteam)) {
+			ps.setInt(1, teamId);
+			ps.executeUpdate();
+		}
+		catch( SQLException e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateTeam(int teamId, String team_name, int team_champs, int divisionId) throws SQLException { //modifies a team based off of team id
+		try ( PreparedStatement ps = connection.prepareStatement(updateteam)) {
+			ps.setInt(4, teamId);
+			ps.setString(1, team_name);
+			ps.setInt(2, team_champs);
+			ps.setInt(3, divisionId);
+			ps.executeUpdate();
+		}
+		catch( SQLException e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Teams getTeamById(int teamId) throws SQLException { //selects a team from the db based off team id
+		PreparedStatement ps = connection.prepareStatement("SELECT * FROM players WHERE teamId = ?");
+		ps.setInt(1, teamId);
 		ResultSet rs = ps.executeQuery();
 		rs.next();
 		return populateTeam(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4)); 
 	}
-	
-	public void createNewTeam(String teamName) throws SQLException {
-		PreparedStatement ps = connection.prepareStatement("INSERT INTO teams(name) VALUES(?)");
-		ps.setString(1, teamName);
-		ps.executeUpdate();
-	}
-	
-	public void deleteTeamById(int id) throws SQLException {
-		playerDao.deletePlayersByTeamId(id);
-		PreparedStatement ps = connection.prepareStatement("DELETE FROM teams WHERE id = ?");
-		ps.setInt(1,  id);
-		ps.executeUpdate();
-	}
-	
-	private Teams populateTeam(int id, String team_name, int team_champs, int divisionId) throws SQLException {
-		return new Teams(id, team_name, team_champs, playerDao.getPlayersByTeamId(id), divisionId);
+
+	private Teams populateTeam(int int1, String string, int int2, int int3) { //probably didn't need this extraneous method
+		return new Teams( int1, string, int2, int3 );
 	}
 }
