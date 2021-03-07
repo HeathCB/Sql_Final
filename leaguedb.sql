f63a4169d6c30c704fcedb03b130dbbf548a243d
Heath C Brown <Heath.c.b@gmail.com>
2021-03-06 23:18:09 -0600

    Update leaguedb.sql
    fixed Id names

--- leaguedb.sql
+++ leaguedb.sql
@@ -1,4 +1,5 @@
-create database if not exists prosports;
+
+		create database if not exists prosports;
 
 use prosports;
 
@@ -8,38 +9,35 @@ drop table if exists divisions;
 drop table if exists leagues;
 
 create table leagues (
-	id int(10) not null auto_increment,
+	leagueId int(10) not null auto_increment,
 	league_name varchar(50) not null,
-	primary key(id)
+	primary key(leagueId)
 );
 
 create table divisions (
-	id int(10) not null auto_increment,
+	divisionId int(10) not null auto_increment,
 	division_name varchar(50) not null,
 	division_champs int(10),
-	league_id int(10) not null,
-	foreign key league_id references leagues(id),
-	primary key(id)
+	leagueId int(10) not null,
+	foreign key (leagueId) references leagues(leagueId),
+	primary key(divisionid)
 );
 
 create table teams (
-	id int(10) not null auto_increment,
+	teamId int(10) not null auto_increment,
 	team_name varchar(50) not null,
 	team_champs int(10),
-	division_id int(10) not null,
-	foreign key division_id references divisions(id),
-	primary key(id)
+	divisionId int(10) not null,
+	foreign key (divisionId) references divisions(divisionId),
+	primary key(teamId)
 );
 
 create table players (
-	id int(10) not null auto_increment,
+	playerId int(10) not null auto_increment,
 	player_name varchar(50) not null,
 	player_num int (2) not null,
 	player_position varchar(50) not null,
-	team_id int(10) not null,
-	foreign key team_id references teams(id),
-	primary key(id)
+	teamId int(10) not null,
+	foreign key (teamId) references teams(teamId),
+	primary key(playerId)
 );
-
-
-		
